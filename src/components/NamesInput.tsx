4b2e8962ad9f16388bc702e89e13a51fb681ffeb
CapSap <cshotam@gmail.com>
2022-12-23 15:04:55 +1100

    basic username component

--- src/components/NamesInput.tsx
+++ src/components/NamesInput.tsx
@@ -1,16 +1,28 @@
 import React, {useState} from 'react';
-import {Text, TextInput, View} from 'react-native';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 
 export const NamesInput: React.FC = () => {
   const [userName, setuserName] = useState('');
+
   return (
     <View>
       <Text>Your Name</Text>
       <TextInput
+        style={styles.input}
         onChangeText={newText => setuserName(newText)}
-        placeholder="asddasdasdsadasads name"
+        placeholder="Your name"
         defaultValue={userName}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: 'white',
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
