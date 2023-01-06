28de9c71c31ec348a5abfd298d811ddebc3b47fb
CapSap <cshotam@gmail.com>
2023-01-06 21:50:04 +1100

    add list of mate names

--- src/components/NamesInput.tsx
+++ src/components/NamesInput.tsx
@@ -1,27 +1,84 @@
 import React, {useState} from 'react';
 import {
+  FlatList,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
   View,
+  SafeAreaView,
 } from 'react-native';
+
+type MateItemProps = {
+  item: string;
+};
+
+const Mate: React.FC<MateItemProps> = ({item}) => {
+  return (
+    <View style={styles.mateContainer}>
+      <Text style={styles.mate}>{item}</Text>
+    </View>
+  );
+};
 
 export const NamesInput: React.FC = () => {
   const [userName, setuserName] = useState('');
+  const [mates, setMates] = useState([
+    'FlatList',
+    'StyleSheet',
+    'Text',
+    'TextInput',
+    'TouchableOpacity',
+    'View',
+    'SafeAreaView',
+    'FlatList',
+    'StyleSheet',
+    'Text',
+    'TextInput',
+    'TouchableOpacity',
+    'View',
+    'SafeAreaView',
+    'FlatList',
+    'StyleSheet',
+    'Text',
+    'TextInput',
+    'TouchableOpacity',
+    'View',
+    'SafeAreaView',
+  ]);
 
   return (
     <View>
-      <Text>Your Name</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={newText => setuserName(newText)}
-        placeholder="Your name"
-        defaultValue={userName}
-      />
-      <TouchableOpacity style={styles.button}>
-        <Text>Save</Text>
-      </TouchableOpacity>
+      <View style={styles.flatListContainer}>
+        <FlatList
+          ListHeaderComponent={
+            <View>
+              <Text>Your Name</Text>
+              <TextInput
+                style={styles.input}
+                onChangeText={newText => setuserName(newText)}
+                placeholder="Your name"
+                defaultValue={userName}
+              />
+              <Text>Your Mates (use a comma to seperate names)</Text>
+              <TextInput
+                style={styles.input}
+                onChangeText={newText => setMates(newText.split(','))}
+                placeholder="Your mates"
+                defaultValue={mates.toString()}
+              />
+            </View>
+          }
+          ListFooterComponent={
+            <TouchableOpacity style={styles.button}>
+              <Text>Save</Text>
+            </TouchableOpacity>
+          }
+          data={mates}
+          renderItem={Mate}
+          keyExtractor={(item, index) => item + index}
+        />
+      </View>
     </View>
   );
 };
@@ -39,4 +96,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#DDDDDD',
     padding: 10,
   },
+  mateContainer: {
+    margin: 10,
+  },
+  mate: {
+    padding: 5,
+    fontSize: 20,
+    borderColor: 'red',
+    borderStyle: 'solid',
+    borderWidth: 1,
+  },
+  flatListContainer: {},
 });
