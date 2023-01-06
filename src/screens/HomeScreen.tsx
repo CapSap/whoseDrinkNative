536adf5c211bbc9bb4a9af2a18e6d51c6787aaa4
CapSap <cshotam@gmail.com>
2023-01-06 21:51:15 +1100

    .

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import {View} from 'react-native';
 import {NamesInput} from '../components/NamesInput';
-import {RoundRecorder} from '../components/RoundRecorder';
 
 export const HomeScreen: React.FC = () => {
   return (
     <View>
       <NamesInput />
-      <RoundRecorder />
     </View>
   );
 };
